4620bf0ef7170c87fe128e7f53620c99f9d7335a
Willbeckh <wileyhndirangu04#gmail.com>
2024-08-16 20:19:46 +0300

    feat: update allpages toggle feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,82 @@
-// import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import Checkbox from "./components/Checkbox";
 
 const App = () => {
+  const [checkedItems, setCheckedItems] = useState({
+    allPages: false,
+    pages: {
+      page1: false,
+      page2: false,
+      page3: false,
+      page4: false,
+      page5: false,
+      page6: false,
+    },
+  });
+
+  // checkboxes state management
+  const handleAllPagesToggle = () => {
+    //toggles state when all pages is clicked
+    const newState = !checkedItems.allPages;
+
+    setCheckedItems({
+      allPages: newState,
+      pages: Object.keys(checkedItems.pages).reduce(
+        // using .reduce to iterate thru pages and return a new pageItem with a newState
+        (acc, key) => ({ ...acc, [key]: newState }),
+        {}
+      ),
+    });
+  };
+
+  // handling individual checkbox
+  const handlePageToggle = (page) => {
+    const newState = !checkedItems.pages[page]; // check & set state of single checkbox
+    setCheckedItems((prevState) => ({
+      ...prevState,
+      pages: {
+        ...prevState.pages,
+        [page]: newState,
+      },
+      allPages: Object.values(prevState.pages).every((item) => item)
+        ? newState
+        : false,
+    }));
+  };
+
+  // page labels for dynamic and scalability
+  const pageLabels = [
+    "Page 1",
+    "Page 2",
+    "Page 3",
+    "Page 4",
+    "Page 5",
+    "Page 6",
+  ];
+
   return (
     <div className="container">
       <div className="content">
         <div>
-          <Checkbox label="All pages" />
+          <Checkbox
+            label="All pages"
+            checked={checkedItems.allPages}
+            onChange={handleAllPagesToggle}
+          />
         </div>
         <hr />
-        
+
         <div className="pageList">
-          <div>
-            <Checkbox label="Page 1" />
-          </div>
-          <Checkbox label="Page 2" />
-          <Checkbox label="Page 3" />
-          <Checkbox label="Page 4" />
-          <Checkbox label="Page 5" />
-          <Checkbox label="Page 6" />
+          {pageLabels.map((label, index) => (
+            <Checkbox
+              key={index}
+              label={label}
+              checked={checkedItems.pages[`page${index + 1}`]}
+              onChange={() => handlePageToggle(`page${index + 1}`)}
+            />
+          ))}
         </div>
         <hr />
 
