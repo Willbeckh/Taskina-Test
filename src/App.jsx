c3640638adf5ea2f21d7ad0c9573262a742c7ccd
Willbeckh <wileyhndirangu04#gmail.com>
2024-08-16 15:12:35 +0300

    Initial boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <h3>All pages</h3>
+        <hr />
+        <div className="page-list">
+          <p>Page 1</p>
+          <p>Page 1</p>
+          <p>Page 1</p>
+          <p>Page 1</p>
+          <p>Page 1</p>
+          <p>Page 1</p>
+        </div>
+        <hr />
+        <button>doneee</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
