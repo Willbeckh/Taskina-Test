6da6e756527ff3282618df76361b869ae73cf9b7
Willbeckh <wileyhndirangu04#gmail.com>
2024-08-16 19:28:49 +0300

    ft: add checkbox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 // import React from "react";
 import "./App.css";
 import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
 
 const App = () => {
   return (
     <div className="container">
       <div className="content">
-        <h3>All pages</h3>
-        <hr />
-        <div className="pageList">
-          <p>Page 1</p>
-          <p>Page 1</p>
-          <p>Page 1</p>
-          <p>Page 1</p>
-          <p>Page 1</p>
-          <p>Page 1</p>
+        <div>
+          <Checkbox label="All pages" />
         </div>
         <hr />
-        <Button color="#fece22" label="Done" />
+        
+        <div className="pageList">
+          <div>
+            <Checkbox label="Page 1" />
+          </div>
+          <Checkbox label="Page 2" />
+          <Checkbox label="Page 3" />
+          <Checkbox label="Page 4" />
+          <Checkbox label="Page 5" />
+          <Checkbox label="Page 6" />
+        </div>
+        <hr />
+
+        <Button color="#ffce22" label="Done" />
       </div>
     </div>
   );
